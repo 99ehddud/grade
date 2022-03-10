2a59e45bb695c7df930ed83af91705780f40dc0c
99ehddud <99ehddud@gmail.com>
2022-03-11 08:53:09 +0900

    Only Fram

---
+++ GradeSystem.java
@@ -0,0 +1,14 @@
+package grade;
+
+import javax.swing.*;
+
+public class GradeSystem {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setSize(1080, 720);
+        
+        f.setLocationRelativeTo(null);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
