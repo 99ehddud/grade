4e5ac3232ebe7148851739a01e5733b0207c2878
99ehddud <99ehddud@gmail.com>
2022-03-12 19:34:08 +0900

    Ver.x.0.1 (Set Frame & Add MenuBar)

--- GradeSystem.java
+++ GradeSystem.java
@@ -1,14 +1,57 @@
 package grade;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
 
-public class GradeSystem {
-    public static void main(String[] args) {
-        JFrame f = new JFrame();
-        f.setSize(1080, 720);
+public class GradeSystem extends JFrame {
+
+    public GradeSystem() {
+        this.setTitle("Grade System");
+        this.setSize(1080, 720);
+
+        createRoot();
+        createPanel();
+
+        this.setLocationRelativeTo(null);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+
+    public void createRoot() {
+        JRootPane root = super.getRootPane();
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu gradeSystem = new JMenu("GradeSystem");
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+
+        gradeSystem.add( new JMenuItem("About") );
+        gradeSystem.addSeparator();
+        gradeSystem.add(new JMenuItem("Exit"));
+
+        file.add( new JMenuItem("New") );
+        file.add( new JMenuItem("Open") );
+
+        edit.add( new JMenuItem("Edit") );
+        edit.add( new JMenuItem("Delete") );
+
+        menuBar.add(gradeSystem);
+        menuBar.add(file);
+        menuBar.add(edit);
+
+        root.setJMenuBar(menuBar);
+    }
+    
+    public void createPanel() {
+        JPanel titlePanel = new JPanel();
         
-        f.setLocationRelativeTo(null);
-        f.setLayout(null);
-        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GradeSystem();
     }
 }
