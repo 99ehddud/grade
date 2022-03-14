3cbee3b45ce32739d1ff06c0665d90dc3d531ccc
99ehddud <99ehddud@gmail.com>
2022-03-14 21:23:27 +0900

    Ver.x.0.3 (Add JTable & JScrollPane)

--- GradeManagementSystem.java
+++ GradeManagementSystem.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.BorderLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -11,35 +12,38 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRootPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
-public class GradeManagementSystem {
+public class GradeManagementSystem extends JFrame {
 
-    public static void main(String[] args) {
+    public GradeManagementSystem() {
         // JFrame Main Setting
-        JFrame mainFrame = new JFrame("Grade Management System");
-        mainFrame.setSize(1080, 720);
-        mainFrame.setBackground(Color.BLACK);
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setResizable(false);
+        setTitle("Grade Management System");
+        setSize(1080, 720);
+        getContentPane().setBackground(new Color(129, 144, 255));
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // JRootPane & JMenuBar Setting
-        JRootPane root = mainFrame.getRootPane();
+        JRootPane root = this.getRootPane();
         JMenuBar menuBar = new JMenuBar();
-        JMenu gradeSystem = new JMenu("GradeSystem");
-        gradeSystem.add(new JMenuItem("About"));
-        gradeSystem.addSeparator();
-        gradeSystem.add(new JMenuItem("Exit"));
+        JMenu system = new JMenu("System");
+        system.add(new JMenuItem("About"));
+        system.addSeparator();
+        system.add(new JMenuItem("Exit"));
+        menuBar.add(system);
         JMenu file = new JMenu("File");
         file.add(new JMenuItem("New"));
         file.add(new JMenuItem("Open"));
+        menuBar.add(file);
         JMenu edit = new JMenu("Edit");
         edit.add(new JMenuItem("Edit"));
         edit.add(new JMenuItem("Delete"));
-        
-        menuBar.add(gradeSystem);
-        menuBar.add(file);
         menuBar.add(edit);
         root.setJMenuBar(menuBar);
 
@@ -48,24 +52,46 @@ public class GradeManagementSystem {
         titlePanel.setSize(1080, 50);
         titlePanel.setBackground(new Color(86, 105, 255));
         titlePanel.setLayout(new BorderLayout());
-        
+
         JLabel title = new JLabel("Grade Management System");
         title.setFont(new Font("Gothic", Font.BOLD, 30));
         title.setForeground(Color.white);
         title.setBorder(new EmptyBorder(0, 30, 0, 0));
         titlePanel.add(title, BorderLayout.WEST);
+        this.add(titlePanel);
 
-        // JPanel(For Main) Setting
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(new Color(129, 144, 255));
-        mainPanel.setBounds(0, 50, 1080, 620);
+        // JPanel(For Table) Setting
+        JPanel tablePanel = new JPanel();
+        tablePanel.setBackground(new Color(129, 144, 255));
+        tablePanel.setBounds(50, 70, 980, 450);
+        tablePanel.setLayout(new BorderLayout());
 
-        mainFrame.add(titlePanel);
-        mainFrame.add(mainPanel);
+        String[] subjectArray = {"No.", "Name", "Language", "Math", "Society", "Science"};
+        DefaultTableModel tableTitle = new DefaultTableModel(subjectArray, 0);
+        JTable mainTable = new JTable(tableTitle);
+        JScrollPane scroll = new JScrollPane(mainTable);
+        scroll.setBorder(BorderFactory.createEmptyBorder());
+        tablePanel.add(scroll);
+
+        this.add(tablePanel);
+
+        // JPanel(For Input) Setting
+        JPanel inputPanel = new JPanel();
+        inputPanel.setBackground(new Color(215, 215, 255));
+        inputPanel.setBounds(50, 520, 980, 130);
+        this.add(inputPanel);
 
         // Floating JFrame on Display
-        mainFrame.setLayout(null);
-        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainFrame.setVisible(true);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GradeManagementSystem();
     }
 }
+
+
+// Add Button for add grade
+// Add TextArea for input name & grade
+// 129, 144, 255
